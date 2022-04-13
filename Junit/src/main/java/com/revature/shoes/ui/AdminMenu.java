faf4fa54fe92fd82a1de61a03e1afbc280945811
Russell W Anderson <92950114+Randerson723@users.noreply.github.com>
2022-04-12 21:54:51 -0500

    adminmenu

--- Junit/src/main/java/com/revature/shoes/ui/AdminMenu.java
+++ Junit/src/main/java/com/revature/shoes/ui/AdminMenu.java
@@ -8,11 +8,11 @@ import com.revature.shoes.services.UserService;
 
 import java.util.Scanner;
 
-public class UserMenu implements Imenu {
+public class AdminMenu implements Imenu {
     private final ShoeService shoeService;
     Shoe shoe = new Shoe();
 
-    public UserMenu(ShoeService shoeService) {
+    public AdminMenu(ShoeService shoeService) {
         this.shoeService = shoeService;
     }
 
@@ -25,10 +25,10 @@ public class UserMenu implements Imenu {
         Scanner sc = new Scanner(System.in);
 
         while(!done) {
-            System.out.println("\nUser/Login Menu");
+            System.out.println("\nAdmin Menu");
             System.out.println("\n(1) User Login ");
             System.out.println("(2) Add Shoe");
-            System.out.println("(3) Checkout");
+            System.out.println("(3) Order History");
             System.out.println("(4) Shoe Menu");
             System.out.println("(5) Leave Store");
 
@@ -59,60 +59,7 @@ public class UserMenu implements Imenu {
 
         }
     }
-    /*private void createUser() {
 
-        char input = ' ';
-        Scanner  sc1 = new Scanner(System.in);
-        User user_1 = new User();
-        boolean exit = false;
-        boolean userOk = false;
-
-        while (!exit) {
-            System.out.println("\nGetting Ready to Create New User......");
-            System.out.println("Enter User ID");
-            user_1.setId(sc1.nextInt());
-
-            System.out.println("\nEnter your first name");
-            user_1.setFirstName(sc1.next());
-
-            System.out.println("Enter you last name");
-            user_1.setLastName(sc1.next());
-
-            System.out.println("Enter your email");
-            user_1.setEmail(sc1.next());
-
-            System.out.println("Enter your Zip Code");
-            user_1.setZipCode(sc1.nextInt());
-
-
-            System.out.println("Enter your Favorite Brand");
-            user_1.setFavBrand(sc1.next());
-
-            System.out.println("Enter your Username");
-            user_1.setUserName(sc1.next());
-
-            while (!userOk) {
-                System.out.println("Is the information entered correct? [y/n]");
-                System.out.println(user_1);
-
-                input = sc1.next().charAt(0);
-
-                switch (input) {
-                    case 'y':
-                        System.out.println("User Created...Go find some Kick's");
-                        userOk = true;
-                        exit = true;
-                        break;
-                    case 'n':
-                        userOk = true;
-                        break;
-                    default:
-                        System.out.println("\nIncorrect Input Try Again");
-                        break;
-                }
-            }
-        }
-    }*/
 
     private void createShoe() {
         String shoe_brand = " ";
